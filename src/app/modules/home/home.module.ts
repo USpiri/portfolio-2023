5162aa4e4a2dfc31c9f3d2bc67d7213e7dbb5bb6
Uriel Spiridione <urielspiridione135@gmail.com>
2023-03-28 01:28:57 -0300

    ✨ Feat: Added code-background

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { CodeBackgroundComponent } from './components/code-background/code-background.component';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CodeBackgroundComponent
   ],
   imports: [
     CommonModule,
