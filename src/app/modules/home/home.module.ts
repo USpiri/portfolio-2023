e6d022f1c6dd2f443242b29a4949bb41cb7af00e
Uriel Spiridione <urielspiridione135@gmail.com>
2023-03-29 00:09:07 -0300

    🟢 Chore: Updateing barrels

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
-import { CodeBackgroundComponent } from './components/landing/components/code-background/code-background.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ExperienceComponent } from './components/experience/experience.component';
@@ -12,6 +11,7 @@ import { BlogComponent } from './components/blog/blog.component';
 import { GalleryComponent } from './components/gallery/gallery.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { AboutComponent } from './components/about/about.component';
+import { CodeBackgroundComponent } from './components/landing/components/code-background/code-background.component';
 
 @NgModule({
   declarations: [
