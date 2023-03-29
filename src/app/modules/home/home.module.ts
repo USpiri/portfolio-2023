f2c0563f438118c232dd26eb9651c27ba9f80127
Uriel Spiridione <urielspiridione135@gmail.com>
2023-03-29 00:02:23 -0300

    🌿 Refactor: Changed project architecture

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,14 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
-import { CodeBackgroundComponent } from './components/code-background/code-background.component';
-
-
+import { CodeBackgroundComponent } from './components/landing/components/code-background/code-background.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { EducationComponent } from './components/education/education.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    CodeBackgroundComponent
+    CodeBackgroundComponent,
+    LandingComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    EducationComponent,
+    BlogComponent,
+    GalleryComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule,
