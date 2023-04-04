6ad586692bf7b72f9bb8e5a87b778ab0120c7442
Uriel Spiridione <urielspiridione135@gmail.com>
2023-04-04 00:42:56 -0300

    ✨ Feat: Added navigation buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'portfolio';
   icon = 'lock';
+  scrollPosition: number = 0;
+  windowHeight: number = 0;
+  documentHeight: number = 0;
+
+  ngAfterViewInit(): void {
+    window.addEventListener('scroll', this.onWindowScroll.bind(this));
+    this.onWindowScroll();
+  }
+
+  onWindowScroll() {
+    this.scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight || 0;
+    this.documentHeight = document.documentElement.scrollHeight || document.body.scrollHeight || 0;
+  }
+
+  scrollToTop() {
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+  }
+
+  scrollToBottom() {
+    window.scroll({ top: document.documentElement.scrollHeight, left: 100, behavior: 'smooth' });
+  }
+  
 }
--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  exports: [MatToolbarModule, MatIconModule],
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+  ],
 })
 export class MaterialModule {}
