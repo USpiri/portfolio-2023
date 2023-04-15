cf42b600e6b8c0d0e22f012d1ea1ca485915bcd4
Uriel Spiridione <urielspiridione135@gmail.com>
2023-04-14 21:43:35 -0300

    ✨ Feat: Moved buttons home module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,11 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent {
   title = 'portfolio';
   icon = 'lock';
-  scrollPosition: number = 0;
-  windowHeight: number = 0;
-  documentHeight: number = 0;
-
-  ngAfterViewInit(): void {
-    window.addEventListener('scroll', this.onWindowScroll.bind(this));
-    this.onWindowScroll();
-  }
-
-  onWindowScroll() {
-    this.scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    this.windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight || 0;
-    this.documentHeight = document.documentElement.scrollHeight || document.body.scrollHeight || 0;
-  }
-
-  scrollToTop() {
-    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
-  }
-
-  scrollToBottom() {
-    window.scroll({ top: document.documentElement.scrollHeight, left: 100, behavior: 'smooth' });
-  }
-  
 }
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+  scrollPosition: number = 0;
+  windowHeight: number = 0;
+  documentHeight: number = 0;
+
+  ngAfterViewInit(): void {
+    window.addEventListener('scroll', this.onWindowScroll.bind(this));
+    this.onWindowScroll();
+  }
+
+  onWindowScroll() {
+    this.scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    this.windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight || 0;
+    this.documentHeight = document.documentElement.scrollHeight || document.body.scrollHeight || 0;
+  }
+
+  scrollToTop() {
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+  }
 
 }
