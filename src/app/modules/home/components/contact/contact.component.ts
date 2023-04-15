55717d00bbe5aada6bd8c582150cec94b1fc0fbc
Uriel Spiridione <urielspiridione135@gmail.com>
2023-04-14 22:13:53 -0300

    ✨ Feat: Added snackbars

--- src/app/modules/home/components/contact/contact.component.ts
+++ src/app/modules/home/components/contact/contact.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-
+  constructor(private snackBar: MatSnackBar) {}
+  sendMail() {
+    this.snackBar.open('Mail sended succesfully!', undefined, {
+      duration: 2000
+    });
+  }
 }
--- src/app/modules/home/shared/material/material.module.ts
+++ src/app/modules/home/shared/material/material.module.ts
@@ -5,6 +5,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   exports: [
@@ -14,6 +15,7 @@ import { MatInputModule } from '@angular/material/input';
     MatProgressSpinnerModule,
     MatFormFieldModule,
     MatInputModule,
+    MatSnackBarModule
   ],
 })
 export class MaterialModule {}
