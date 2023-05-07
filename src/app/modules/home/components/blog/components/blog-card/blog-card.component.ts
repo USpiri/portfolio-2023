e6d7ead66f743b458a1ab0d0fa529015656ba71b
Uriel Spiridione <urielspiridione135@gmail.com>
2023-05-07 01:36:10 -0300

    ✨ Feat: Added blog card

--- src/app/modules/home/components/blog/components/blog-card/blog-card.component.ts
+++ src/app/modules/home/components/blog/components/blog-card/blog-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Blog } from '@models';
 
 @Component({
   selector: 'app-blog-card',
   templateUrl: './blog-card.component.html',
-  styleUrls: ['./blog-card.component.scss']
+  styleUrls: ['./blog-card.component.scss'],
 })
 export class BlogCardComponent {
-
+  @Input() blog: Blog = {
+    title: '',
+    description: '',
+    author: '',
+    date: new Date(),
+    tags: [],
+    link: '',
+  };
 }
