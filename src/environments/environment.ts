4d3dc30b63976ba88dc6c60588c51f4d93a01daa
Uriel Spiridione <urielspiridione135@gmail.com>
2023-05-09 18:03:59 -0300

    🌿 Refactor: Adding environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,2 +1,9 @@
+const baseUrl = '';
+
 export const environment = {
+  API_USER: baseUrl + '',
+  API_AUTH: baseUrl + '',
+  API_SKILL: baseUrl + '',
+  API_PROJECT: baseUrl + '',
+  API_EXPERIENCE: baseUrl + '',
 };
