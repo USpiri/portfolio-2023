885f02c2a6b8d7d559bfc01f770c626eef3a5ca3
Uriel Spiridione <urielspiridione135@gmail.com>
2023-05-09 19:23:26 -0300

    ✨ Feat: Connected to User Service

--- src/app/modules/home/components/landing/landing.component.ts
+++ src/app/modules/home/components/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { USER } from '@assets/data/user.mock';
 import { User } from '@models';
+import { UserService } from '@modules/home/shared/services/user.service';
 
 @Component({
   selector: 'app-landing',
@@ -9,4 +10,7 @@ import { User } from '@models';
 })
 export class LandingComponent {
   user: User = USER;
+  constructor(private userService: UserService) {
+    this.userService.user$.subscribe((user) => (this.user = user));
+  }
 }
