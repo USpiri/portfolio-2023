9d12ce41c052b91e260ed91e8e94be43ff14c4f0
Uriel Spiridione <urielspiridione135@gmail.com>
2023-05-09 21:27:41 -0300

    ✨ Feat: Added method to close session when token expire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@shared/service/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  constructor(private authService: AuthService) {
+    if (this.authService.isLoggedIn()) {
+      this.authService.logout();
+    }
+  }
 }
