5243e8624aadb7ce0d099635c0d4a892a92ce1b0
Uriel Spiridione <urielspiridione135@gmail.com>
2023-05-30 01:54:00 -0300

    🐛 Fix: Endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  API: 'https://uriel-spiridione-backend.vercel.app/',
+  API: 'https://uriel-spiridione-backend.vercel.app',
   USER_ID: '64754a07e510d60911acad8b',
 };
